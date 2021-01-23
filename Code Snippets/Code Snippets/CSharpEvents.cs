c47b9628de52eb66cf91f4ddd980102afafebf3b
Asutosh Mittapalli <asutoshmittapalli@gmail.com>
2021-01-23 06:56:21 +0530

    First Commit

---
+++ Code Snippets/Code Snippets/CSharpEvents.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Code_Snippets
+{
+    public delegate void Notify();
+    class ProcessBusinessLogic
+    {
+        public event Notify ProcessCompleted;
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+
+            OnProcessCompleted();
+        }
+
+        protected virtual void OnProcessCompleted()
+        {
+            ProcessCompleted?.Invoke();
+        }
+    }
+
+    class Subscriber
+    {
+        public static void Main()
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted;
+            bl.StartProcess();
+        }
+
+        // event handler
+        public static void bl_ProcessCompleted()
+        {
+            Console.WriteLine("Process Completed!");
+        }
+    }
+}
