10e8d350efc9afcbe4f345c3c7dee91b90aaaf53
Asutosh Mittapalli <asutoshmittapalli@gmail.com>
2021-01-23 07:15:57 +0530

    Built in Event Handler Delegate

---
+++ Code Snippets/Code Snippets/BuiltInEventHandlerDelegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_Snippets
+{
+    class EventHandlerDelegateSubscriber
+    {
+        public static void Main()
+        {
+            EventHandlerDelegateProcessBusinessLogic bl = new EventHandlerDelegateProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted;
+            bl.StartProcess();
+        }
+
+        // event handler
+        public static void bl_ProcessCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Process Completed!");
+        }
+    }
+
+    public class EventHandlerDelegateProcessBusinessLogic
+    {
+        public event EventHandler ProcessCompleted;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+            
+            OnProcessCompleted(EventArgs.Empty);
+        }
+
+        protected virtual void OnProcessCompleted(EventArgs e)
+        {
+            ProcessCompleted?.Invoke(this, e);
+        }
+    }
+}
